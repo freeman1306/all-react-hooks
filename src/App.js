49c0135afcd937ca57e704997f12347e63fdab63
freeman1306 <zavalkin06@gmail.com>
2020-08-28 22:17:23 +0300

    done

--- src/App.js
+++ src/App.js
@@ -1,19 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
-import Main from './Main';
-import {AlertProvider} from './alert/AlertContext';
-import Alert from './alert/Alert';
 
+function useInput(initialValue) {
+  const [value, setValue] = useState(initialValue);
+
+  const onChange = (e) => {
+    setValue(e.target.value);
+  };
+
+  const clear = () => setValue('');
+
+  return {
+    bind: { value, onChange },
+    value,
+    clear,
+  };
+}
 
 function App() {
- 
+  const input = useInput('');
+
   return (
-      <AlertProvider>
-      <div className="container">
-      <Alert/>
-      <Main toggle={()=> {}}/>
-      </div>
-      </AlertProvider>
+    <div className="container">
+      <input type="text" {...input.bind} />
+      <button className="btn btn-warning" onClick={input.clear}>
+        Clean
+      </button>
+
+      <h1>{input.value}</h1>
+    </div>
   );
 }
 
